c8b6811ec4b1bc3a710a00d42e5285403595ef1a
Jack Henson <jackhenson1@gmail.com>
2023-03-19 10:50:47 -0400

    Updated ERB view files and main ruby file

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,17 +2,69 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
+helpers do
+  def slugify(text)
+    text.downcase.gsub(/\s+/, "-").gsub(/[^\w-]/, "")
+  end
+
+  def in_paragraphs(text)
+    text.split("\n\n").each_with_index.map do |line, idx|
+      "<p id=paragraph#{idx}>#{line}</p>"
+    end.join
+  end
+
+ def each_chapter
+    @contents.each_with_index do |name, idx|
+      number = idx+1
+      contents = File.read("data/chp#{number}.txt")
+      yield number, name, contents
+    end
+  end
+
+  def chapters_matching(query)
+    results = []
+
+    return results if !query || query.empty?
+
+    each_chapter do |number, name, contents|
+      matches = {}
+      contents.split("\n\n").each_with_index do |paragraph, idx|
+      matches[idx] = paragraph if paragraph.include?(query)
+      end
+      results << {number: number, name: name} if contents.include?(query)
+    end
+
+    results
+  end
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
   
   erb :home 
 end
 
-get "/chapters/1" do
-  @title = "Chapter 1"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp1.txt")
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number-1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read("data/chp#{number}.txt")
 
   erb :chapter
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
